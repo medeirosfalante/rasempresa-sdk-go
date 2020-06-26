3ead5bb63ab9c351cd0468a7aa396cd60b95f58e
Rafael Medeiros <rafaelgurudesign@gmail.com>
2020-06-26 02:16:26 -0300

    get subaccount

--- Account.go
+++ Account.go
@@ -16,6 +16,34 @@ type SubAccountCreate struct {
 	Telefone string `json:"telefone"`
 }
 
+type BalanceAccountRequest struct {
+	Subconta string `json:"subconta"`
+}
+
+type BalanceAccountResponse struct {
+	COD      string       `json:"COD"`
+	Mensagem string       `json:"Mensagem"`
+	Cadastro *AccountData `json:"cadastro"`
+	Wallets  wallets      `json:"wallets"`
+	Saldos   saldos       `json:"saldos"`
+}
+
+type AccountData struct {
+	ID       string `json:"id"`
+	Nome     string `json:"nome"`
+	Email    string `json:"email"`
+	Data     string `json:"data"`
+	Telefone string `json:"telefone"`
+	Cpf      string `json:"cpf"`
+	Rg       string `json:"rg"`
+	Banco    string `json:"banco"`
+	Ag       string `json:"ag"`
+	Conta    string `json:"conta"`
+}
+
+type wallets map[string]string
+type saldos map[string]string
+
 //SubAccountResponse - struct de resposta ao criar uma subconta
 type SubAccountResponse struct {
 	COD      string `json:"COD"`
@@ -35,6 +63,22 @@ func (p AccountService) CreateSubAccount(subaccount *SubAccountCreate) (*SubAcco
 		return nil, nil, errors.New("subaccount is null")
 	}
 	err, errAPI := p.client.Request("POST", "/v1/privado/criar_subconta", subaccount, &response)
+	if err != nil {
+		return nil, nil, err
+	}
+	if errAPI != nil {
+		return nil, errAPI, nil
+	}
+	return response, nil, nil
+}
+
+//BalanceSubAccount - detalhes e saldo da subconta
+func (p AccountService) BalanceSubAccount(subconta string) (*BalanceAccountResponse, *Error, error) {
+	var response *BalanceAccountResponse
+	if subconta == "" {
+		return nil, nil, errors.New("subconta is empty")
+	}
+	err, errAPI := p.client.Request("POST", "/v1/privado/exibir_balanco_subconta", &BalanceAccountRequest{Subconta: subconta}, &response)
 	if err != nil {
 		return nil, nil, err
 	}
--- Account_test.go
+++ Account_test.go
@@ -2,6 +2,7 @@ package ras_test
 
 import (
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -23,5 +24,64 @@ func TestCreateAccount(t *testing.T) {
 	if response == nil {
 		t.Error("response is null")
 		return
+	}
+}
+
+func TestBalanceSubAccount(t *testing.T) {
+	godotenv.Load(".env.test")
+	client := ras.New(os.Getenv("KEY"), os.Getenv("SECRET"), os.Getenv("ENV"))
+	response, errAPI, err := client.AccountService().BalanceSubAccount(os.Getenv("SUBACCOUNT_TEST"))
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
+	}
+}
+
+func TestBalanceSubAccountValidAllAddress(t *testing.T) {
+	godotenv.Load(".env.test")
+	client := ras.New(os.Getenv("KEY"), os.Getenv("SECRET"), os.Getenv("ENV"))
+	response, errAPI, err := client.AccountService().BalanceSubAccount(os.Getenv("SUBACCOUNT_TEST"))
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
+	}
+	re := regexp.MustCompile("^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$")
+	if !re.MatchString(response.Wallets["BTC"]) {
+		t.Error("invalid BTC address")
+		return
+
+	}
+	re = regexp.MustCompile("^([X7])[a-zA-Z0-9]{33}$")
+	if !re.MatchString(response.Wallets["DASH"]) {
+		t.Error("invalid DASH address")
+		return
+	}
+	re = regexp.MustCompile("^0x[a-fA-F0-9]{40}$")
+	if !re.MatchString(response.Wallets["RAS"]) {
+		t.Error("invalid RAS address")
+		return
+
+	}
+	re = regexp.MustCompile("^0x[a-fA-F0-9]{40}$")
+	if !re.MatchString(response.Wallets["USDT"]) {
+		t.Error("invalid USDT address")
+		return
+
 	}
 }
