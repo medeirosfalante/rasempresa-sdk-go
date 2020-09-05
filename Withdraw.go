d26e6733c8ad322e860ef6fa4fc2589b347658e0
Rafael Medeiros <rafaelgurudesign@gmail.com>
2020-09-05 17:12:04 -0300

    add withdraw create and withdraw list

--- Withdraw.go
+++ Withdraw.go
@@ -5,6 +5,11 @@ type WithdrawService struct {
 	client *APIClient
 }
 
+//WithdrawService - Instanciar serviço de saque
+func (c *APIClient) WithdrawService() *WithdrawService {
+	return &WithdrawService{client: c}
+}
+
 //WithdrawCreate - struct para criar uma retirada
 type WithdrawCreate struct {
 	Taxa   int    `json:"taxa"`
@@ -32,6 +37,36 @@ type FeeItemResponse struct {
 	Fast    string `json:"FAST"`
 }
 
+// WithdrawListQuery - query for list \n  0 =  todos, 1 =  em analise, 2  = aprovados, 3  = reprovados
+type WithdrawListQuery struct {
+	Filtro int    `json:"filtro"`
+	ID     string `json:"id_retirada"`
+}
+
+//WithdrawItem - item do saque
+type WithdrawItem struct {
+	ID            string `json:"id_retirada"`
+	Data          string `json:"data"`
+	Hora          string `json:"hora"`
+	Moeda         string `json:"moeda"`
+	Valor         string `json:"valor"`
+	Taxa          string `json:"taxa"`
+	ValorRecebido string `json:"valor_recebido"`
+	Comprovante   string `json:"comprovante"`
+	Wallet        string `json:"wallet"`
+	Banco         string `json:"banco"`
+	Conta         string `json:"conta"`
+	Agencia       string `json:"agencia"`
+	Status        string `json:"status"`
+}
+
+// WithdrawListResponse - Retorno da lista de retiradas
+type WithdrawListResponse struct {
+	COD      string          `json:"COD"`
+	Data     []*WithdrawItem `json:"data"`
+	Mensagem string          `json:"Mensagem"`
+}
+
 // CreateWithdraw - criar um saque
 func (p WithdrawService) CreateWithdraw(withdraw *WithdrawCreate) (*WithdrawResponse, *Error, error) {
 	var response *WithdrawResponse
@@ -45,15 +80,23 @@ func (p WithdrawService) CreateWithdraw(withdraw *WithdrawCreate) (*WithdrawResp
 	return response, nil, nil
 }
 
-//WithdrawService - Instanciar serviço de saque
-func (c *APIClient) WithdrawService() *WithdrawService {
-	return &WithdrawService{client: c}
-}
-
 // Fees - lista o fee
 func (p WithdrawService) Fees() (*FeeResponse, *Error, error) {
 	var response *FeeResponse
 	err, errAPI := p.client.Request("POST", "/v1/privado/taxas_retirada", nil, &response)
+	if err != nil {
+		return nil, nil, err
+	}
+	if errAPI != nil {
+		return nil, errAPI, nil
+	}
+	return response, nil, nil
+}
+
+// Fees - lista o fee
+func (p WithdrawService) ListWithdraw(query *WithdrawListQuery) (*WithdrawListResponse, *Error, error) {
+	var response *WithdrawListResponse
+	err, errAPI := p.client.Request("POST", "/v1/privado/listar_retiradas_empresa", query, &response)
 	if err != nil {
 		return nil, nil, err
 	}
--- withdraw_test.go
+++ withdraw_test.go
@@ -33,8 +33,62 @@ func TestWithdraw(t *testing.T) {
 		Taxa:   2,
 		Moeda:  "RAS",
 		Wallet: os.Getenv("WALLET_RAS_WITHDRAW"),
-		Valor:  "10",
+		Valor:  "100",
 	})
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
+	}
+}
+
+func TestWithdrawList(t *testing.T) {
+	godotenv.Load(".env.test")
+	client := ras.New(os.Getenv("KEY"), os.Getenv("SECRET"), os.Getenv("ENV"))
+	response, errAPI, err := client.WithdrawService().ListWithdraw(&ras.WithdrawListQuery{Filtro: 0})
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
+	}
+}
+
+func TestWithdrawListPending(t *testing.T) {
+	godotenv.Load(".env.test")
+	client := ras.New(os.Getenv("KEY"), os.Getenv("SECRET"), os.Getenv("ENV"))
+	response, errAPI, err := client.WithdrawService().ListWithdraw(&ras.WithdrawListQuery{Filtro: 1})
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
+	}
+}
+
+func TestWithdrawListIDQuery(t *testing.T) {
+	godotenv.Load(".env.test")
+	client := ras.New(os.Getenv("KEY"), os.Getenv("SECRET"), os.Getenv("ENV"))
+	response, errAPI, err := client.WithdrawService().ListWithdraw(&ras.WithdrawListQuery{Filtro: 0, ID: os.Getenv("WITHDRAWID_FILTER_TEST")})
 	if err != nil {
 		t.Errorf("err : %s", err)
 		return
